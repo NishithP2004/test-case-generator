539dd6beb65e3cd30dcb2493f821dfd82d2dfe0b
Nishith P <nishithp11@gmail.com>
2026-02-26 16:18:11 +0530

    Add arguments to browser launch options

--- utils.js
+++ utils.js
@@ -82,7 +82,11 @@ async function fetchProblemFromLeetcode(url) {
             defaultViewport: {
                 width: 1920,
                 height: 1080
-            }
+            },
+            args: [
+                "--no-sandbox",
+                "--disable-setuid-sandbox"
+            ]
         });
         const page = await browser.newPage();
         
